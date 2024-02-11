ff7e4b3aba75a55ec530ed2f94a8c42d3c9d0b92
eujin-shin <eujinshin0124@gmail.com>
2024-02-11 12:17:11 +0900

    #1 feat: add Login page UI

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+
+const InputDiv = styled.div`
+  position: relative;
+  width: 390px;
+  padding-top: 20px;
+  display: flex;
+  flex-direction: row;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  color: #000;
+  font-size: 18px;
+  text-align: center;
+`;
+
+export default function TextInput({ title, value, setValue, style, type }) {
+  return (
+    <InputDiv>
+      <Text>{title}</Text>
+      <input
+        type={!!type ? type : "text"}
+        style={{
+          width: "280px",
+          height: "55px",
+          border: "none",
+          fontSize: "18px",
+          paddingLeft: "10px",
+          ...style,
+        }}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </InputDiv>
+  );
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import styled from "styled-components";
+import TextInput from "../components/TextInput";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding-top: 10vh;
+`;
+
+const LoginDiv = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  width: 550px;
+  height: 550px;
+  padding-top: 120px;
+  padding-bottom: 100px;
+  margin: 50px auto;
+  border-radius: 100%;
+  background-color: #d9d9d9;
+`;
+
+const SubmitBtn = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  margin: 50px auto;
+  width: 220px;
+  height: 55px;
+  border-radius: 20px;
+  background-color: white;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export default function Login() {
+  const [loginForm, setLoginForm] = useState({ id: "", pw: "" });
+
+  const handleSubmit = () => {
+    console.log(loginForm.id);
+    console.log(loginForm.pw);
+  };
+
+  return (
+    <Wrapper>
+      <span style={{ margin: "20px" }}>함께 지구를 지키세요!</span>
+      <LoginDiv>
+        <span>LOGIN</span>
+        <div
+          style={{
+            marginTop: "20px",
+          }}
+        >
+          <TextInput
+            title="ID"
+            value={loginForm.id}
+            setValue={(value) => {
+              setLoginForm((prev) => {
+                return { ...prev, id: value };
+              });
+            }}
+          />
+          <TextInput
+            title="Password"
+            type="password"
+            value={loginForm.pw}
+            setValue={(value) => {
+              setLoginForm((prev) => {
+                return { ...prev, pw: value };
+              });
+            }}
+          />
+          <SubmitBtn onClick={handleSubmit}>
+            <span
+              style={{
+                margin: "auto",
+                fontSize: "18px",
+                width: "100%",
+                lineHeight: "100%",
+              }}
+            >
+              Login
+            </span>
+          </SubmitBtn>
+        </div>
+      </LoginDiv>
+      <div>
+        <span>회원가입</span>
+      </div>
+    </Wrapper>
+  );
+}
