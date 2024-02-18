a726af985f1c5e45595f8429edf16e671adf490a
eujin-shin <eujinshin0124@gmail.com>
2024-02-18 15:51:07 +0900

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Login /> },
+  { path: "/chat", element: <Chat /> },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <Chat />
+    <div id="App">
+      <RouterProvider router={router} />
     </div>
   );
 }
